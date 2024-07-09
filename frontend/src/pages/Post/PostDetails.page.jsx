bcdb571aa58fb49eccea0ab42b728fea899b6fad
bebotrobotics921 <jessica.tc.woo@gmail.com>
2024-07-08 20:36:24 -0700

    last change - postsdetail page not working

--- frontend/src/pages/Post/PostDetails.page.jsx
+++ frontend/src/pages/Post/PostDetails.page.jsx
@@ -1,34 +1,34 @@
 import DOMAIN from "../../services/endpoint";
 import axios from "axios";
 import { Link } from 'react-router-dom';
-import { Button, Container, SimpleGrid } from "@mantine/core";
+import { Button, Container } from "@mantine/core";
 import { useLoaderData } from "react-router-dom";
 
 
 function PostDetailsPage() {
   const posts = useLoaderData();
-   console.log(posts)
+   
   return (
-    
+    <>
       <Container >
+        <h1>{posts.title}</h1>
+        <p>{posts.category}</p>
+        <p>{posts.content}</p>
+          <img src={posts.image} alt={posts.title}/>
+         
+      <Button><Link to="/posts/id/edit">Edit</Link></Button>
+      <br></br>
+      <br></br>
+      <Button><Link to="/posts">Back To Posts</Link></Button>
+      
         
-        <SimpleGrid cols={2}>
-        
-      <Link to="/posts/id/edit">
-      <Button>Edit</Button>
-      </Link>
-        
-          
-          
-        </SimpleGrid>
       </Container>
-    
+      </> 
   );
 }
 
 export const postDetailsLoader = async ({ params }) => {
-  const res = await axios.get(`${DOMAIN}/api/posts/${params.id}`)
-  console.log(res.data)
+  const res = await axios.get(`${DOMAIN}/api/posts/${params.id}`);
   return res.data;
 };
 
